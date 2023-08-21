916d15fa0cc0b2f2705312ff77d523ba4d0cb478
elvispresniy <129598369+elvispresniy@users.noreply.github.com>
2023-08-21 18:34:25 +0500

    Add files via upload

---
+++ script.py
@@ -0,0 +1,41 @@
+import webparser
+
+original_url = r"https://www.planetminecraft.com/projects/tag/bundle/?order=order_popularity&share=schematic&p="
+save_path = r"Enter path where .schematic will be stored"
+
+start = int(input('Input start page: '))
+end = int(input('Input end page: '))
+
+urls = webparser.get_urls(original_url, start, end)
+
+for url in urls:
+    # Get response from a page
+    response = webparser.get_response(url)
+
+    if not response.status_code == 200:
+        continue
+
+    # Get all links from a page
+    links = webparser.get_map_links(response)
+
+    # Iterate through links
+    for link in links:
+
+        # Get response from a map
+        map_response = webparser.get_map_response(link)
+
+        if not map_response.status_code == 200:
+            continue
+
+        # Get download link
+        download_link = webparser.get_download_link(map_response)
+
+        # Download and get a filepath
+        filepath = webparser.installation(download_link, save_path)
+
+        if filepath is not None:
+            if '.zip' in filepath:
+                # Unpack if .zip
+                webparser.unpack_zip(filepath, save_path)
+
+webparser.wipe_folders(save_path)
